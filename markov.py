ff2c1cc71200a7def6c49b88d20395a1511cf3c4
Jona Haga-Camacho <jona.haga@gmail.com>
2014-03-31 21:33:24 -0700

    Cleaned up comments

--- markov.py
+++ markov.py
@@ -9,62 +9,40 @@ from random import randint
 def make_chains(corpus):
     """Takes an input text as a string and returns a dict of
     markov chains."""
-    # return {}
-    # Split text into list of words
     words = corpus.split()
 
-    # Create an empty dict
     d = {}
 
-    # Traverses list of words builds the dict
     for i in range(len(words) - 2):
-        # Create tuple of a word and the following word as a key
         key = (words[i], words[i + 1])
-        # Check to see if the tuple/key is in the dict
         if not d.get(key):
-            # If it's not in the dict, add it as a new value list
             d[key] = [words[i + 2]]
         else:
-            # If it's in the dict, append to values list
             d[key].append(words[i + 2])
 
-    # for key in d:
-    #     print "Key: %s,\t\t\tValue: %s" % (key, d[key])
-    
     return d 
 
 def make_text(chains):
     """Takes a dict of markov chains and returns random text
     based off an original text."""
-    #return "Here's some random text."
-
-    # Return a list of dict keys
     keys = chains.keys()
-
     words = []
-    # Finding a capitalized word to start first tuple
-    while len(words) == 0:   
-        # Create a random number start to index into the list of keys
-        start = randint(0, len(keys) - 1)
 
-        # Pull random key from list of keys
+    while len(words) == 0:   
+        start = randint(0, len(keys) - 1)
         key = keys[start]
 
-        # Make sure first item in first tuple is a capilized letter
         if ord(key[0][0]) >= ord('A') and ord(key[0][0]) <= ord('Z'):
             words = [key[0], key[1]]
 
     end_punct = ".!?"
 
-    # Getting the next word
     while chains.get(key) and (len(' '.join(words)) < 25 or end_punct.find(key[1][-1]) == -1):
         rand_number = randint(0, len(chains[key]) - 1)
         rand_word = chains[key][rand_number]        
 
-        # Append random word to words list value
         words.append(rand_word)
 
-        # Updating key to be the last two items in list
         key = tuple((words[-2:]))
 
     sentence = ' '.join(words)
